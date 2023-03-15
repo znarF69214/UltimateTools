58cb54c5bf7f27864e73f3634b05bc2039d91d44
znarF69214 <znarf69214@gmail.com>
2023-03-15 18:27:14 +0100

    Set Version correctly

--- src/main/java/com/znarfmods/ultimatetools/References.java
+++ src/main/java/com/znarfmods/ultimatetools/References.java
@@ -3,6 +3,6 @@ package com.znarfmods.ultimatetools;
 public class References {
 	public static final String MODID = "ultimatetools";
     public static final String NAME = "UltimateToolsMod";
-    public static final String VERSION = "1.0.0.5";
+    public static final String VERSION = "1.0.0.5.1";
     public static final String MCVERSION = "1.12.2";
 }
